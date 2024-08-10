46ff4bc148f44c7667a76025a1649a2c591da3d3
Taidev7-11 <23701841.tai@student.iuh.edu.vn>
2024-08-11 01:26:49 +0700

    add first prj

--- lab1/src/Welcome.java
+++ lab1/src/Welcome.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Tan Tai 23701841 DHKTPM19B - 420300359104");
+    }
 }
